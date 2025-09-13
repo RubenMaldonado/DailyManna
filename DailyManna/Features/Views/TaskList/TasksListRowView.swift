0ad04b32abe877803e753acd286ccba9786ff779
Ruben Maldonado Tena <ruben@maldonadotena.com>
2025-09-13 12:34:48 -0400

    Fix: define local DateFormatter.shortDate/shortDateTime in TasksListRowView to avoid fileprivate access

--- DailyManna/Features/Views/TaskList/TasksListRowView.swift
+++ DailyManna/Features/Views/TaskList/TasksListRowView.swift
@@ -120,4 +120,20 @@ private struct CompactDuePill: View {
     }
 }
 
+// Local date formatters for compact due pill
+private extension DateFormatter {
+    static let shortDate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    static let shortDateTime: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
+
 
